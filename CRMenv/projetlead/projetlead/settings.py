cc81ed126ae60f60f05d16dd8e3f0cce7c15c2ab
ghizlane <bayaghizla@gmail.com>
2024-08-02 00:09:10 +0100

    ajouter des application avec modification et ajouter ces application dans  le settings

--- CRMenv/projetlead/projetlead/settings.py
+++ CRMenv/projetlead/projetlead/settings.py
@@ -38,6 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lead',
+    'analytics',
+    'appointments',
+    'campaigns',
+    'clients',
+    'communication',
+    'members',
+    'reports'
 ]
 
 MIDDLEWARE = [
