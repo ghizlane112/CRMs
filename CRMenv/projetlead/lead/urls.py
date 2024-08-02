95b84082cd40db671673e58004015f418c7a5647
ghizlane <bayaghizla@gmail.com>
2024-08-02 13:24:55 +0100

    commit the div of button

--- CRMenv/projetlead/lead/urls.py
+++ CRMenv/projetlead/lead/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.one, name='one'),
+
     path('two',views.two,name='two'),
+    path('three',views.three,name='three'),
     path('leadlist', views.lead_list, name='lead_list'),
     path('lead/<int:pk>/', views.lead_detail, name='lead_detail'),
     path('lead/new/', views.lead_create, name='lead_create'),
